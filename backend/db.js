3c54e909181990a6785d7df5fae4ae4438dc95f2
Jose <juegosaplicaciones61@gmail.com>
2026-05-22 10:45:37 -0500

    debug mysql

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,8 @@
 const mysql = require("mysql2");
+console.log(process.env.MYSQLHOST);
+console.log(process.env.MYSQLUSER);
+console.log(process.env.MYSQLDATABASE);
+console.log(process.env.MYSQLPORT);
 
 const connection = mysql.createConnection({
   host: process.env.MYSQLHOST,
